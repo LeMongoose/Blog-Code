0ff78ee066b2feb2bbe29cf17e9775a32ab3cb73
Calum Cookson <calum.cookson@gmail.com>
2020-02-11 11:49:39 +1100

    delaunay

--- delaunay.py
+++ delaunay.py
@@ -7,6 +7,39 @@ def sequence(n,d):
     for i in range(n):
         yield int ((i&(2**d))/(2**d));
 
+class Delaunay:
+    def __init__(self, P, D=None):
+        self.P = P
+        if D == None:
+            D = len(P[0])
+
+        if D < 2:
+            raise Exception
+
+        self.D = D
+        
+        if len(P[0]) != self.D:
+            raise Exception
+        
+        return
+
+    def __box(self):
+        _mn = min(self.P,0)
+        _mx = max(self.P,0)
+        _bounds = np.zeros(self.D,2)
+        _bounds[:,0] = _mn
+        _bounds[:,1] = mx
+        _box = np.zeros(self.D, self.D**2)
+        for _d in range(self.D):
+            _box[d,:] = sequence(self.D**2, d)
+        return _box
+
+    def __great_triangle(self):
+        return
+
+    def __call__(self):
+        return
+
 def main():
     D = 3
     d = 2
--- test_delaunay.py
+++ test_delaunay.py
@@ -1,17 +1,61 @@
 
 import unittest
 import delaunay
-
+import numpy as np
 class TestDelaunay(unittest.TestCase):
+
+    def generate_points(self, N,D):
+        for _p in np.random.rand(N,D):
+            yield tuple(_p)
+        
+    def test_delaunay_init_infer_D(self):
+        N = 5
+        D = 2
+        P = list(self.generate_points(N,D))
+        _delaunay = delaunay.Delaunay(P)
+
+        # infer D from P if no D arg
+        self.assertEquals(_delaunay.D, D, msg="infer D from P if no D arg")
+        
+    def test_delaunay_init_D_eq_dimP(self):
+        N = 5
+        D = 2
+        P = list(self.generate_points(N,D))
+        
+        # if provided D should be same order as P, if not throw exception
+        self.assertRaises(Exception, delaunay.Delaunay, P, D+1 )
+        _delaunay = delaunay.Delaunay(P,D)
+    
+    def test_delaunay_init_D_gteq_2(self):
+        N = 5
+        D = 1
+        P = list(self.generate_points(N,D))
+        
+        # if provided D should be same order as P, if not throw exception
+        self.assertRaises(Exception, delaunay.Delaunay, P, D )
+        
+
+    def test_box(self):
+        return
+        #_delaunay = delaunay.Delaunay(P)
 
     def test_sequence(self):
         params = [
             (2,0, [0,1]),
             (4,0, [0,1,0,1]),
-            (4,1, [0,0,1,1])
+            (4,1, [0,0,1,1]),
+            (8,0, [0,1,0,1,0,1,0,1]),
+            (8,1, [0,0,1,1,0,0,1,1]),
+            (8,2, [0,0,0,0,1,1,1,1]),
+            (16,0, [0,1,0,1,0,1,0,1, 0,1,0,1,0,1,0,1]),
+            (16,1, [0,0,1,1,0,0,1,1, 0,0,1,1,0,0,1,1]),
+            (16,2, [0,0,0,0,1,1,1,1, 0,0,0,0,1,1,1,1]),
+            (16,3, [0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1])
         ]
         for (n,d,s) in params:
-            assertSequenceEqual(list(delaunay.sequence(n,d)), s )
+            #print("{0} {1} {2} {3}".format(n,d,s, list(delaunay.sequence(n,d))))
+
+            self.assertSequenceEqual(list(delaunay.sequence(n,d)), s)
 
 
 
